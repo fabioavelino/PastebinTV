b434f7442b77c79b653ddefdf5481cf055302017
Fabio Avelino <>
2019-11-25 01:38:39 +0100

    Looping on 5 first urls for init Player

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pastebintv/blocs/channels_bloc/bloc.dart';
 import 'package:flutter/services.dart';
+import 'package:pastebintv/models/channel.dart';
 import 'package:video_player/video_player.dart';
 
 class PlayerScreen extends StatefulWidget {
@@ -32,15 +33,28 @@ class PlayerScreenState extends State<PlayerScreen> {
     super.dispose();
   }
 
+  void initPlayer(Channel channel, int index) {
+    if (index >= 5) {
+      return;
+    }
+    _controller = VideoPlayerController.network(channel.urls[index])
+      ..initialize().then((_) {
+        _controller.play();
+        return;
+      }).catchError((onError) {
+        initPlayer(channel, index + 1);
+      }).timeout(Duration(seconds: 3), onTimeout: () {
+        initPlayer(channel, index + 1);
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     final ChannelsBloc channelsBloc = BlocProvider.of<ChannelsBloc>(context);
 
     return BlocBuilder<ChannelsBloc, ChannelsState>(builder: (context, state) {
       if (state.isPlayerInitialized == false) {
-        _controller =
-            VideoPlayerController.network(state.channelSelected.urls[0])
-              ..initialize().then((_) => _controller.play());
+        initPlayer(state.channelSelected, 0);
         channelsBloc.add(PlayerInitalized());
       }
 
