dc237aedc007facb4492392e42a7ec90a4485d91
Fabio Avelino <>
2019-12-16 21:55:16 +0100

    Adding Focus for ChannelWidget

--- pastebintvApp/lib/widgets/channel_widget.dart
+++ pastebintvApp/lib/widgets/channel_widget.dart
@@ -9,27 +9,32 @@ class ChannelWidget extends StatelessWidget {
   ChannelWidget(this.title, this.icon, this.background, this.onPressed);
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () {
-          print("on tap");
-          onPressed();
-        },
-        child: Container(
-            child: Row(
-              children: [
-                /* Stack(children: [
+    return Focus(child: Builder(builder: (BuildContext context) {
+      final FocusNode focusNode = Focus.of(context);
+      final bool hasFocus = focusNode.hasFocus;
+      return GestureDetector(
+          onTap: () {
+            print("on tap");
+            onPressed();
+          },
+          child: Container(
+              child: Row(
+                children: [
+                  /* Stack(children: [
               background != null ? Image.network(background) : null,
               icon != null ? Image.network(icon) : null
             ]), */
-                Center(child: Text(title)),
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.all(Radius.circular(10))),
-            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 0),
-            width: 150,
-            margin: EdgeInsets.all(20)));
+                  Center(child: Text(title)),
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+              decoration: BoxDecoration(
+                  color: hasFocus ? Colors.red : Colors.transparent,
+                  border: Border.all(),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 0),
+              width: 150,
+              margin: EdgeInsets.all(20)));
+    }));
   }
 }
